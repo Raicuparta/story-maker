fb281ff0b0643eb83406ca2c42cf4a760c147b50
Raicuparta <raicuparta@gmail.com>
2019-05-26 18:03:35 +0200

    Updates

---
+++ precache-manifest.1648e7bcc25fdded39753522c33d98ee.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd99bca5504bb48a40e5615172addec4",
+    "url": "/story-builder/index.html"
+  },
+  {
+    "revision": "464576a9dcac39d75d52",
+    "url": "/story-builder/static/js/2.2e91b532.chunk.js"
+  },
+  {
+    "revision": "9c8031b4b1f1e65eeba5",
+    "url": "/story-builder/static/js/main.2a2a8c18.chunk.js"
+  },
+  {
+    "revision": "e203f0c0364d9bd7acfe",
+    "url": "/story-builder/static/js/runtime~main.dc2ea323.js"
+  }
+]);
