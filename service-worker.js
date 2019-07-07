2e80da4e0c31a00257986a30a3aaa5132bd9ece6
Raicuparta <raicuparta@gmail.com>
2019-07-07 17:08:48 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/story-maker/precache-manifest.48dfa5c973bfa8f07fa9d53dfcdf90cb.js"
+  "/story-maker/precache-manifest.ce9b933ba4ac888e1cd5a172eca86a65.js"
 );
 
 self.addEventListener('message', (event) => {
