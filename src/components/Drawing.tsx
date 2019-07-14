6570c61a2b3fcc13a5b701ef1d6700271d957d0a
Raicuparta <raicuparta@gmail.com>
2019-07-14 11:59:40 +0200

    Discrete pixel drawing

--- src/components/Drawing.tsx
+++ src/components/Drawing.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import bresenham from '../bresenham';
 
 import Colors from '../styles/colors';
 import {
@@ -6,8 +7,8 @@ import {
 } from '../styles/Drawing.style';
 
 const viewBoxSize = {
-  width: 400,
-  height: 300,
+  width: 100,
+  height: 75,
 };
 
 const Drawing: React.FC<{
@@ -17,122 +18,113 @@ const Drawing: React.FC<{
   onChange,
   dataURL,
 }) => {
-  const [context, setContext] = useState<CanvasRenderingContext2D>();
-  const [canvas, setCanvas] = useState<HTMLCanvasElement>();
-  const [isDrawing, setIsDrawing] = useState(false);
-  const [prevPosition, setPrevPosition] = useState<Point>();
+    const [context, setContext] = useState<CanvasRenderingContext2D>();
+    const [canvas, setCanvas] = useState<HTMLCanvasElement>();
+    const [points, setPoints] = useState<Point[]>([]);
+    const [isDrawing, setIsDrawing] = useState(false);
+    const [prevPosition, setPrevPosition] = useState<Point>();
 
-  useEffect(() => {
-    function clearCanvas() {
-      if (!context || !canvas) return;
+    useEffect(() => {
+      function clearCanvas() {
+        if (!context || !canvas) return;
 
-      context.fillStyle = Colors.secondary;
-      context.fillRect(0, 0, canvas.width, canvas.height);
-    }
+        context.fillStyle = Colors.secondary;
+        context.fillRect(0, 0, canvas.width, canvas.height);
+      }
 
-    if (dataURL && context) {
-      const image = new Image();
-      image.onload = () => {
-        clearCanvas();
-        context.drawImage(image, 0, 0);
-      };
-      image.src = dataURL;
-    } else {
       clearCanvas();
+
+      if (points && context) {
+        context.fillStyle = Colors.primary;
+        for (let point of points) {
+          context.fillRect(point.x, point.y, 1, 1)
+        }
+      } else {
+        clearCanvas();
+      }
+    }, [context, canvas, points]);
+
+    function relativePoint(event: React.MouseEvent | React.Touch): Point {
+      const {
+        width,
+        height,
+        left,
+        top,
+      } = canvas!.getBoundingClientRect();
+
+      const viewBoxRatio = viewBoxSize.height / viewBoxSize.width;
+      const boundsRatio = height / width;
+
+      const scale = viewBoxRatio < boundsRatio
+        ? width / viewBoxSize.width
+        : height / viewBoxSize.height
+
+      const offset: Point = {
+        x: left,
+        y: top,
+      };
+
+      if (viewBoxRatio > boundsRatio) {
+        offset.x += (width - (viewBoxSize.width * scale)) / 2;
+      } else {
+        offset.y += (height - (viewBoxSize.height * scale)) / 2;
+      }
+
+      return {
+        x: Math.floor((event.clientX - offset.x) / scale),
+        y: Math.floor((event.clientY - offset.y) / scale),
+      };
     }
-  }, [context, canvas, dataURL]);
 
-  function relativePoint(event: React.MouseEvent | React.Touch) : Point {
-    const {
-      width,
-      height,
-      left,
-      top,
-    } = canvas!.getBoundingClientRect();
+    function setCanvasRef(instance: HTMLCanvasElement | null) {
+      if (!instance || canvas || context) return;
 
-    const viewBoxRatio = viewBoxSize.height / viewBoxSize.width;
-    const boundsRatio = height / width;
-
-    const scale = viewBoxRatio < boundsRatio
-      ? width / viewBoxSize.width
-      : height / viewBoxSize.height
-
-    const offset = {
-      x: 0,
-      y: 0,
-    };
-
-    if (viewBoxRatio > boundsRatio) {
-      offset.x += (width - (viewBoxSize.width * scale)) / 2;
-    } else {
-      offset.y += (height - (viewBoxSize.height * scale)) / 2;
+      setCanvas(instance);
+      setContext(instance.getContext('2d') || undefined);
     }
 
-    return {
-      x: (event.clientX - offset.x) / scale - left,
-      y: (event.clientY - offset.y) / scale - top,
-    };
-  }
+    function handleMouseMove(mouseEvent: React.MouseEvent) {
+      if (!isDrawing || !canvas || !context) return;
 
-  function setCanvasRef(instance: HTMLCanvasElement | null) {
-    if (!instance || canvas || context) return;
+      const position: Point = relativePoint(mouseEvent);
 
-    setCanvas(instance);
-    setContext(instance.getContext('2d') || undefined);
-  }
+      setPoints(prevPoints => [
+        ...prevPoints,
+        ...bresenham(prevPosition || position, position),
+      ]);
 
-  function handleMouseMove(mouseEvent: React.MouseEvent) {
-    if (!isDrawing || !canvas || !context) return;
-
-    const position: Point = relativePoint(mouseEvent);
-
-    // OK hear me out.
-    // There's a bug in Chrome that makes canvas fail to update if the updated
-    // area is outside the canvas' original bounds, without taking into account
-    // the resizing / translation done by css property content-fit: contain.
-    // So I need to update this extra point in the origin, to make sure the
-    // canvas always refreshes as it should.
-    context.fillRect(0, 0, 1, 1);
-
-    context.beginPath();
-    context.moveTo((prevPosition || position).x, (prevPosition || position).y);
-    context.lineTo(position.x, position.y);
-    context.strokeStyle = Colors.primaryVariant;
-    context.lineWidth = 1;
-    context.stroke();
-
-    setPrevPosition(position);
-  }
-  function handleMouseDown() {
-    setIsDrawing(true);
-  }
-  function handleMouseUp() {
-    setIsDrawing(false);
-    setPrevPosition(undefined);
-
-    if (canvas && isDrawing && onChange) {
-      onChange(canvas.toDataURL());
+      setPrevPosition(position);
     }
-  }
-  function handleMouseOut() {
-    handleMouseUp();
-  }
-  function handleMouseEnter() {
+    function handleMouseDown() {
+      setIsDrawing(true);
+    }
+    function handleMouseUp() {
+      setIsDrawing(false);
+      setPrevPosition(undefined);
 
-  }
+      if (canvas && isDrawing && onChange) {
+        onChange(canvas.toDataURL());
+      }
+    }
+    function handleMouseOut() {
+      handleMouseUp();
+    }
+    function handleMouseEnter() {
 
-  return (
-    <Canvas
-      ref={setCanvasRef}
-      onMouseMove={handleMouseMove}
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-      onMouseOut={handleMouseOut}
-      onMouseEnter={handleMouseEnter}
-      width={viewBoxSize.width}
-      height={viewBoxSize.height}
-    />
-  );
-};
+    }
+
+    return (
+      <Canvas
+        ref={setCanvasRef}
+        onMouseMove={handleMouseMove}
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+        onMouseOut={handleMouseOut}
+        onMouseEnter={handleMouseEnter}
+        width={viewBoxSize.width}
+        height={viewBoxSize.height}
+      />
+    );
+  };
 
 export default Drawing;
--- src/styles/Drawing.style.ts
+++ src/styles/Drawing.style.ts
@@ -4,9 +4,10 @@ import Colors from './colors';
 
 export const Canvas = styled.canvas`
   margin: 5px;
-  cursor: crosshair;
+  /* cursor: crosshair; */
   background: ${Colors.secondaryVariant};
   border-radius: 10px;
   flex: 1;
   object-fit: contain;
+  image-rendering: pixelated;
 `
